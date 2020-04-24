73ea1a260d9b528d31ecee07503f39059f91f195
maedeh <maedeh.almasi98@gmail.com>
2020-04-24 20:00:19 +0430

    delete some route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,8 +22,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('admin/user','admin\AdminUserController');
