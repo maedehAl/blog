4c196a18e6c48a35921de7a787f30dfbc58182f5
maedeh <maedeh.almasi98@gmail.com>
2020-04-28 21:44:32 +0430

    user view & controller

---
+++ resources/views/backend/user/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0 text-dark">داشبورد</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-left">
+                            <li class="breadcrumb-item"><a href="#">خانه</a></li>
+                            <li class="breadcrumb-item active">داشبورد ورژن 2</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+    </div>
+    @endsection
